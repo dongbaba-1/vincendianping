22f51fbc2f27b0520b8c66a1a211a4de75ca6aee
471732342@qq.com <471732342@qq.com>
2024-05-09 01:24:38 +0800

    5.9

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserInfoServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserInfoServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
 import com.hmdp.entity.UserInfo;
 import com.hmdp.mapper.UserInfoMapper;
 import com.hmdp.service.IUserInfoService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -14,7 +22,11 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-24
  */
+@Slf4j
 @Service
 public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> implements IUserInfoService {
+
+
+
 
 }
